ea0eb3cb336276bc98c7fea28e407d2d27484456
hussainkhushi786 <hussainkhushi601@gmail.com>
2023-06-05 01:19:57 -0700

    i change the position of navbar to fixed top

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -7,7 +7,7 @@ const CustomNavbar = () => {
     <Container fluid>
       <Row>
         <Col className='px-0'>
-          <Navbar bg="light " expand="lg" className="px-4 sticky-top">
+          <Navbar bg="light " expand="lg" className="px-4 fixed-top">
           <Navbar.Brand href="#">CODISTAN</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarSupportedContent" />
           <Navbar.Collapse id="navbarSupportedContent">
